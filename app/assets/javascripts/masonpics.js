bec2db901674def12f0970501fd5f54d584c5f47
perez karjee <pcx1256@gmail.com>
2014-12-26 03:50:37 -0800

    mason

--- app/assets/javascripts/masonpics.js
+++ app/assets/javascripts/masonpics.js
@@ -1,3 +1,10 @@
+var container = document.querySelector('#picsmasonry');
+var msnry;
+// initialize Masonry after all images have loaded
+imagesLoaded( container, function() {
+  msnry = new Masonry( container );
+});
+
 $(document).ready(function() {
   $('#picsmasonry').masonry({
    columnWidth: 320,
